f392a4815ac578c5e49a9fbd542fe5ded8a5c6d8
MaximDudnichenko <maximkus@mail.ru>
2022-09-19 17:37:39 +0300

    Add array input method and array output method

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// the method returns an array of strings entered by the user
+string[] FillArray(int value)
+{
+    string[] result = new string[value];
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива строк: ");
+        result[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+    return result;
+}
+
+// the method outputs an array of strings to the screen
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"['{String.Join("', '", array)}']");
+    Console.WriteLine();
+}
+
